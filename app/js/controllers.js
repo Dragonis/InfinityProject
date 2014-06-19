7bbbea9beae25b330457ce94f5075209100c652b
Dragonis <wojtek.sasiela@gmail.com>
2014-06-19 14:15:41 +0200

    Wstępne dane o postaci

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,37 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', [])
-  .controller('HomeCtrl', ['$scope', function($scope) {
+var myApp = angular.module('myApp.controllers', []);
 
-  }])
-  .controller('HeroCtrl', ['$scope', function($scope) {
+  myApp.factory('Hero',function(){
+     var Hero = {};
+      // opis
+      Hero.id = 1;
+      Hero.nick = "Dragonis";
+      Hero.plec = "kobieta";
+      Hero.rasa = "człowiek";
+      // atrybuty
+      Hero.sila = 4;
+      Hero.zrecznosc = 4;
+      Hero.inteligencja = 4;
+      Hero.witalnosc = 4;
+      // umiejetnosci
+      Hero.walka_wrecz = 1;
+      Hero.kamehameha = 1;
+
+      Hero.dodajwSile = function(){
+          return Hero.sila++;
+      };
+
+      Hero.odemijwSile = function(){
+          return Hero.sila--;
+      };
+      return Hero;
+  });
+  myApp.controller('HomeCtrl', ['$scope', function($scope) {
+
+  }]);
+  myApp.controller('HeroCtrl', ['$scope','Hero', function($scope,Hero) {
+        $scope.bohater = Hero;
 
   }]);
